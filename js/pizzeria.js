d10f14c5d430062ecacb816f07bbf147a8a69f2d
Midori Mori <zaseaweed@gmail.com>
2013-11-04 23:02:43 -0800

    no message

--- js/pizzeria.js
+++ js/pizzeria.js
@@ -3,10 +3,18 @@
 //Document on ready
 $(function() {
 	var menu = $(".row col-md-6"); //grabs 2 columns in row
-	console.log("should be 2" + menu.length);
+	console.log("should be 2: " + menu.length);
+
 	var i; //iterator
 	var item; //current menu item
 	for (i = 0; i < com.dawgpizza.menu.pizzas.length; i++) {
-		console.log(i);
+		var pizzaType; //Meat or Vegetarian
+		item = com.dawgpizza.menu.pizzas[i]; //Current pizza
+		if(item.vegetarian) {
+			pizzaType = 'vegetarian';
+		} else {
+			pizzaType = 'meat';
+		}
+		console.log(pizzaType);
 	}
 });
