b8947a68eb13000be49741a96fe49e497edee60a
Midori Mori <zaseaweed@gmail.com>
2013-11-05 01:31:20 -0800

    no message

--- js/pizzeria.js
+++ js/pizzeria.js
@@ -52,7 +52,7 @@ function otherMenus(menuType) {
 	var place; //Location to append item
 	//console.log(com.dawgpizza.menu.drinks);
 	 for (i = 0; i < com.dawgpizza.menu.drinks.length; i++) {
-	 	item = com.dawgpizza.menu.menuType[i];
+	 	item = com.dawgpizza.menu.drinks[i];
 	 	itemName = $(document.createElement('li'));
 	 	itemName.html(item.name);
 	 	console.log(item.name);
